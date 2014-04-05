a6e43efbb2eaecfc5fd7062a2a79b868051e1bb7
Sebastian Georgi <sgeorgi@sgeorgi.de>
2014-04-05 21:19:15 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yarora::Application.config.secret_key_base = 'a3e1b2fb7e792628d556e4111773c8ac49099d46d5fd4c7a30c9b70118ba0532b27082fafa0c664af049ea3d1ad1bc37eb4d10e2ac7d8f0fb08fe33a684411e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yarora::Application.config.session_store :cookie_store, key: '_yarora_session'
