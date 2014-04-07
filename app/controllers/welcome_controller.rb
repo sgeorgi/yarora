0cf979441a91b00e4b632f916f8190043383c7bc
Sebastian Georgi <sgeorgi@sgeorgi.de>
2014-04-07 15:02:54 +0200

    Added Feature-alert for Features Page redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
 
   def features
+    redirect_to root_path, alert: 'Feature Disabled' if FeatureToggle.off?(:feature_page)
   end
 
   def index
+    puts flash[:warn]
   end
 end
