805653dd0caea415fcab0777c70793a49cc73b98
Sebastian Georgi <sgeorgi@sgeorgi.de>
2014-04-10 13:18:48 +0200

    More pending scenarios for Devise feature spec

---
+++ spec/features/devise_feature_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+# email = 'test@test.com'
+# password = 'ABC123xyz!!'
+
+feature 'Devise', devise_feature: true do
+  scenario 'Existing User logs in and logs out' do
+    visit '/'
+    click_on I18n.t('header.user.login')
+
+    fill_in :user_email, with: 'test@test.com'
+    fill_in :user_password, with: 'ABC123xyz!!'
+
+    click_on 'login_button'
+    expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
+
+    click_link I18n.t('header.user.logout')
+    expect(page).to have_content(I18n.t('devise.sessions.signed_out'))
+  end
+
+  scenario 'User registers' do
+    visit '/'
+    click_link I18n.t('header.user.signup')
+
+    fill_in :user_email, with: 'test2@test.com'
+    fill_in :user_password, with: 'ABC123xyz!!'
+    fill_in :user_password_confirmation, with: 'ABC123xyz!!'
+
+    click_button I18n.t('devise.ui.sign_up')
+    expect(page).to have_content(I18n.t('devise.registrations.signed_up_but_unconfirmed'))
+  end
+
+  scenario 'User changes password' do
+    pending
+    expect(true).to be_false
+  end
+
+  scenario 'User changes email address and confirms' do
+    pending
+    expect(true).to be_false
+  end
+
+  scenario 'User forgets password' do
+    pending
+    expect(true).to be_false
+  end
+
+  scenario 'User requests another cofirmation mail' do
+    pending
+    expect(true).to be_false
+  end
+end
--- spec/views/layouts/_top_bar.html.slim_spec.rb
+++ spec/views/layouts/_top_bar.html.slim_spec.rb
@@ -42,4 +42,17 @@ describe 'layouts/_top_bar.html.slim' do
     render
     expect(rendered).to include(I18n.t('header.user.profile'))
   end
+
+  it 'does show a link to signup when not logged in' do
+    allow(view).to receive(:user_signed_in?).and_return(false)
+    render
+    expect(rendered).to include(I18n.t('header.user.signup'))
+  end
+
+  it 'does not show a link to signup when logged in ' do
+    allow(view).to receive(:user_signed_in?).and_return(true)
+    render
+    expect(rendered).not_to include(I18n.t('header.user.signup'))
+  end
+
 end
